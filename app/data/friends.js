cf1f166f610ebf7c8cfe58eb63334c5f32819e91
Dominic Fumbanks <fumbanksd@gmail.com>
2018-02-21 16:56:28 -0500

    message

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+var friendList=
+[
+{
+ "name": "Ahmed",
+ "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+"scores": [
+ "5",
+ "1",
+ "4",
+"4",
+ "5",
+ "1",
+"2",
+ "5"
+ "4",
+ "1"
+ ]
+ },
+ {
+ "name": "Stacie Meierhoefer",
+ "photo": "https://www.linkedin.com/in/stacie-meierhoefer-7802a35/",
+ "scores": [
+  "4",
+     "2",
+ "5",
+ "1",
+     "3",
+ "2",
+     "2",
+"1",
+"3",
+"2"
+   ]
+  },
+{
+ "name": "Tom Cruise",
+ "photo": "http://www.worldstopmost.com/wp-content/uploads/2016/04/Tom-Cruise-Most-Handsome-Man-2018.jpg",
+"scores": [
+ "1",
+ "1",
+ "2",
+ "2",
+ "4",
+"4",
+ "3",
+ "3",
+"5",
+     "5"
+ ]
+ },
+ {
+ "name": "Dara",
+ "photo": "https://s3-media4.fl.yelpcdn.com/buphoto/t-4XEYLwmiADJO4FD9trHQ/o.jpg",
+"scores": [
+  "1",
+ "3",
+ "5",
+"2",
+"4",
+ "1",
+     "3",
+ "4",
+ "5",
+ "3"
+ ]
+},
+ {
+  "name": "Brad Pitt",
+   "photo": "https://pbs.twimg.com/media/CO0d2ZcUkAA6gjw.jpg",
+ "scores": [
+  "4",
+ "2",
+"4",
+ "1",
+ "5",
+ "2",
+ "2",
+ "3",
+"1"
+ "4"
+ ]
+},
+ {
+ "name": "Mark Wahlberg",
+ "photo": "http://cinemafeel.com/wp-content/uploads/2017/04/28e355e05f584eeccb3fbeb53bd71ad7.jpg",
+ "scores": [
+ "3",
+"4",
+ "2",
+ "3",
+ "2",
+"1",
+ "3",
+ "2",
+ "2",
+ "5"
+     ]
+},
+{
+ "name": "Sponge Bob",
+ "photo": "static.tvgcdn.net/mediabin/galleries/shows/s_z/si_sp/spongebob_squarepants/season1/sponge-bob-square-pants11.jpg",
+"scores": [
+ "1",
+ "4",
+ "3",
+"2",
+ "1",
+     "5",
+ "3",
+ "1",
+ "2",
+ "4"
+]
+ },
+    {
+ "name": "Captain America",
+ "photo": "http://static.srcdn.com/wp-content/uploads/Captain-America-Glamour.jpg",
+ "scores": [
+ "1",
+     "2",
+     "3",
+     "4",
+     "5",
+     "5",
+     "4",
+"3",
+    "2",
+    "1"
+ ]
+ },
+ {
+ "name": "Captain Underpants",
+"photo": "http://screencrush.com/files/2016/12/a-captain-underpants-movie-is-on-the-way-03.jpg",
+"scores": [
+ "1",
+  "2",
+ "3",
+ "4",
+ "5",
+"1",
+ "2",
+ "3",
+ "4",
+ "5"
+     ]
+}
+ ]
+
+module.exports=friendList;
